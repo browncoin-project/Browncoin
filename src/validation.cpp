b025ea2f70a39f40eeac22bb487e70c668bf4f67
Mahan <mahan.rafiei@yahoo.com>
2020-09-17 19:12:29 +0430

    add correct info about halving

--- src/validation.cpp
+++ src/validation.cpp
@@ -1160,7 +1160,7 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
         return 0;
 
     CAmount nSubsidy = 50 * COIN;
-    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
+    // Subsidy is cut in half every 690,000 blocks which will occur approximately every 1.3 years.
     nSubsidy >>= halvings;
     return nSubsidy;
 }
